59739bbf368ee09f28000f204618c41936a783cb
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-02-14 19:14:06 -0300

    🚀 feat: integrate Service API and start Home page layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import api from "../../service/api";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function loadMovies() {
+      const res = await api.get("movie/now_playing", {
+        params: {
+          api_key: "2e963f4c3e1b02490fac8abe4f9fddcb",
+          language: "en-US",
+          page: 1,
+        },
+      });
+
+      let movies = res.data.results.slice(0, 10);
+      console.log(movies);
+      setMovies(movies);
+    }
+
+    loadMovies();
+  }, []);
+
   return (
-    <div>Welcome Home</div>
-  )
+    <div>
+      <div className="container">
+        <div className="movieList">
+          {movies.map((movie) => {
+            return (
+              <div key={movie.id} className="movie">
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <h2>{movie.title}</h2>
+                <p>{movie.overview}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
